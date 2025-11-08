dc7a5a83c652c7be9272076c26bbd853fd95c35b
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-11-08 15:53:42 +0100

    Area added

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,10 @@
+# Calculate the area of a triangle 
+
+length  = 10
+width = 5
+
+def area(length, width): 
+    area = length * width
+    return print(f"The area of the rectangle is: {area}")
+
+area(length, width)
