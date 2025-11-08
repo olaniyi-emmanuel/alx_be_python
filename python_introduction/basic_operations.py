76a433409b37f9fba494dcce9edff6c3fbb75396
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-11-08 15:40:12 +0100

    Python Workload added

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,21 @@
+#Learning Python syntax 
+#  
+
+#Subtraction of 10 and 5 is 5
+#Multiplication of 10 and 5 is 50
+
+number1 = 5
+number2 = 10
+
+# Addition 
+def addition(number1, number2):
+    return print(f"Addition of {number1} and {number2} is {number1 + number2}")
+addition(number1, number2)
+
+def subtraction(number1, number2):
+    return print(f"Subtraction of {number1} and {number2} is {number1 - number2}")
+subtraction(number1, number2)
+
+def multiplication(number1, number2):
+    return print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
+multiplication(number1, number2)
