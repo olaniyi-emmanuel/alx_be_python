8a9268ccfc5458b5b3f8ddf3b1d1c6cd0bf49728
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-11-08 17:14:32 +0100

    Correction made to saving now monthly_savings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,8 +3,8 @@
 def monthly_tracker(): 
     monthly_income = int(input("Enter your monthly income: "))
     monthly_expenses = int(input("Enter your total monthly expenses: "))
-    savings = monthly_income - monthly_expenses 
-    projected_savings = savings *  12 + (savings * 12 * 0.05)
+    monthly_savings = monthly_income - monthly_expenses 
+    projected_savings = monthly_savings *  12 + (monthly_savings * 12 * 0.05)
     return print(f"Projected savings after one year, with interest, is: ${projected_savings}")
 
 monthly_tracker()
