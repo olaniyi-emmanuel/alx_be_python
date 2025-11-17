d665982837c443ab355cdb00ca86e9861f77fedb
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-11-17 17:30:44 +0100

    simple calculator

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,26 @@
+"""
+Simple Calculator with Match Case
+
+"""
+def simple_calculator(): 
+    num1 = int(input("Enter the first number:"))
+    num2 = int(input("Enter the second number:"))
+
+    operation = input(" Choose the operation (+, -, *, /):")
+
+    match operation: 
+        case "+": 
+            result = num1/num2
+            return print(f"The result is {result}")
+        case "-":
+            result = num1/num2
+            return print(f"The result is {result}")
+        case "/": 
+            result = num1/num2
+            return print(f"The result is {result}")
+        case "*": 
+            result = num1*num2
+            return print(f"The result is {result}")
+    
+
+simple_calculator()
