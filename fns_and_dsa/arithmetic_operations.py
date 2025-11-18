ed92d0f1f1643dd0c0e59ba43937dc5f01e8fd46
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-11-19 00:13:56 +0100

    Function now working

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,14 @@
+"""
+Arithmetic Operations Function
+"""
+
+def perform_operation(num1, num2, operation): 
+    if operation == "add": 
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2 
+    elif operation == "multiply": 
+        return num1 * num2
+    elif operation == "divide": 
+        return num1 / num2 
+    
