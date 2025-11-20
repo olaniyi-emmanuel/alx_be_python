d658b8aa094d424e0e32211341e05a31e7ae16ac
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-11-20 11:50:58 +0100

    shopping Cart

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,41 @@
+"""
+Shopping List Manager
+
+"""
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            item_name = str(input("Enter the name of the item selected: ")).lower()
+            shopping_list.append(item_name)
+            pass
+        elif choice == '2':
+            # Prompt for and remove an item
+            if choice in shopping_list: 
+                shopping_list.remove(choice)
+            pass
+        elif choice == '3':
+            # Display the shopping list
+            for item in shopping_list: 
+                print(item)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
