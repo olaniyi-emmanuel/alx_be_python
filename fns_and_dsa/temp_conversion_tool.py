513c4b179c73a14b221149ca60c045b66e84309f
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-11-20 17:53:38 +0100

    temp conversion

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,14 +1,14 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
-CONSTANT = 32
+
 
 def convert_to_celsius(fahrenheit): 
-    convert_to_celsius = (fahrenheit -CONSTANT) * FAHRENHEIT_TO_CELSIUS_FACTOR 
+    convert_to_celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR 
     return print(f"{fahrenheit}\u00B0F is {convert_to_celsius}\u00B0C") 
 
 def convert_to_fahrenheit(celsius): 
-    convert_to_fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + CONSTANT
+    convert_to_fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     return print(f"{celsius}\u00B0C is {convert_to_fahrenheit}\u00B0F")
 
 while True: 
@@ -19,7 +19,7 @@ while True:
     if not temp_value:
         print('Invalid temperature degree ')
     else:
-        
+
         if temp == "C":
             convert_to_fahrenheit(temp_value)
         elif temp == "F": 
