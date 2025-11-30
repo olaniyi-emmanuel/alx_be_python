c06b38ef6e5c36031c5edac29ce51b310ccfb581
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-11-30 18:19:32 +0100

    Added Unittest class

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,21 @@
+import unittest
+from simple_calculator import SimpleCalculator 
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    #def test_addition(self):
+        """Test the addition method."""
+        #self.assertEqual(self.calc.add(2, 3), 5)
+        #self.assertEqual(self.calc.add(-1, 1), 0)
+        # Add more assertions to thoroughly test the add method.
+
+    def test_substraction(self): 
+        self.assertEqual(self.calc.subtract(12, 10), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
