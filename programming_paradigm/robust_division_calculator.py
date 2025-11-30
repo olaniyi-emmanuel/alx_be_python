761f1f537323c5cf324152a545918861f298535e
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-11-30 13:25:45 +0100

    safe divide

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,13 @@
+def safe_divide(numerator, denominator): 
+    try: 
+        new_numerator = float(numerator)
+        new_denominator = float(denominator)
+    except ValueError: 
+        return print("Error: Please enter numeric values only.")
+    
+    try:
+        return print(f"The result of the division is {new_numerator /new_denominator}")
+    except ZeroDivisionError:
+        print(f"Error: Cannot divide by zero")
+
+safe_divide("ten", 0)
