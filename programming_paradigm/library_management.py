464fbdf25a133c3fa751e38c5a262c4e3d7f4deb
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-12-01 14:37:02 +0100

    Now ready for commit

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -8,9 +8,11 @@ class Book:
     def __str__(self):
         return f"{self.title} {self.author}"
 
-    def return_book(self, title):
+    def return_book(self):
         if self.__is_checked_out:
             self.__is_checked_out = False
+            return True
+        return False
 
     def check_out(self):
         if not self.__is_checked_out:
