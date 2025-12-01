d67ba7a9d4b1d9b9137f1c1c6da2c1cca912899f
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-12-01 22:52:10 +0100

    Now ready for commit

--- oop/book_class.py
+++ oop/book_class.py
@@ -5,7 +5,7 @@ class Book:
         self.year = year
 
     def __del__(self):
-        return  f"Deleting {self.year}"
+        return  print(f"Deleting {self.year}")
 
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
