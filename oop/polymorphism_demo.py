d6a5b798ae6723e094050ebef00c77e6b3ecea59
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-12-02 23:59:19 +0100

    Library is up to date

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,24 @@
+import  math
+class Shape:
+    def __init__(self):
+        pass
+    def area(self):
+        raise NotImplementedError("You forgot to override the area() method!")
+
+class Rectangle(Shape):
+    def __init__(self, length:float, width:float):
+        super().__init__()
+        self.length = length
+        self.width = length
+
+    def area(self):
+        return  self.length * self.width
+
+class Circle(Shape):
+    def __init__(self, radius:float):
+        super().__init__()
+        self.radius = radius
+
+        def area():
+            return  math.pi * self.radius * self.radius
+
