89fa08b197db3b53c33fb2cc8601f867b1a6ec4c
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-12-02 16:19:20 +0100

    Library is up to date

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,16 +3,24 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"{self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self, title:str, author: str, file_size: int):
         super().__init__(title,author)
         self.file_size = file_size
 
+    def __str__(self):
+        return f"{self.title} by {self.author} with {self.file_size} bytes"
+
 class PrintBook(Book):
 
-        def __init__(self, title:str, author:str, page_count:int):
-            super().__init__(title, author)
+    def __init__(self, title:str, author:str, page_count:int):
+        super().__init__(title, author)
+        self.page_count = page_count
+    def __str__(self):
+        return f"{self.title} by {self.author} with total pages of {self.page_count}"
 
 
 class Library:
