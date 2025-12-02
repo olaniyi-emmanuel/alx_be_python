6d4043b3b0278221ae569ded3bdfb29f0f5193c0
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-12-02 17:22:15 +0100

    Library is up to date

--- oop/library_system.py
+++ oop/library_system.py
@@ -37,11 +37,11 @@ class Library:
     def list_books(self):
         for book in self.books:
             if isinstance(book, Book):
-                print(f"This is a book with the title {book.title}")
+                print(f"Book:{book.title} by {book.author}")
             elif isinstance(book, EBook):
-                print(f"This is an ebook with the title {book.title}")
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
             elif isinstance(book, PrintBook):
-                print(f"This is an printbook with the title {book.title}")
+                print(f"PrintBook: {book.title} by  {book.author}, Page Count: {book.page_count}")
             else:
                 print("This is not a book")
 
