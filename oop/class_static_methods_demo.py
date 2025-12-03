3ad744039891226a46d3501dd11aaa0c619102a4
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-12-03 12:05:47 +0100

    Library is up to date

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,15 @@
+class Calculator:
+    calculation_type = "Arithmetic Operations"
+
+    def __init__(self):
+        pass
+
+
+
+    @staticmethod
+    def add(a, b):
+        return  a + b
+    @classmethod
+    def multiply(cls, a, b):
+        return a * b
+
